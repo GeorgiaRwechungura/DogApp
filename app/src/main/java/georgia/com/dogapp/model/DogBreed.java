1bca09f14a7ae2406b9f00333b8f608fd201bf76
Georgia Rwechungura <grwechungura@aimgroup.co.tz>
2020-03-10 18:22:15 +0300

    Setting up the room DogBreed Model

--- app/src/main/java/georgia/com/dogapp/model/DogBreed.java
+++ app/src/main/java/georgia/com/dogapp/model/DogBreed.java
@@ -1,23 +1,42 @@
 package georgia.com.dogapp.model;
 
+import androidx.room.ColumnInfo;
+import androidx.room.PrimaryKey;
+
 import com.google.gson.annotations.SerializedName;
 
 public class DogBreed {
+
+    @ColumnInfo(name = "bree_id")
     @SerializedName("id")
     public String breeId;
+
+    @ColumnInfo(name = "dog_name")
     @SerializedName("name")
-    public  String dogBreed;
+    public String dogBreed;
+
+    @ColumnInfo(name = "life_span")
     @SerializedName("life_span")
-    public  String lifeSpan;
+    public String lifeSpan;
+
+    @ColumnInfo(name = "breed_group")
     @SerializedName("breed_group")
-    public  String breedGroup;
+    public String breedGroup;
+
+    @ColumnInfo(name = "bred_for")
     @SerializedName("bred_for")
     public String bredFor;
+
+    @ColumnInfo(name = "temperament")
     @SerializedName("temperament")
     public String temperament;
+
+    @ColumnInfo(name = "url")
     @SerializedName("url")
-    public  String imageUrl;
-    public  int uid;
+    public String imageUrl;
+
+    @PrimaryKey(autoGenerate = true)
+    public int uid;
 
     public DogBreed(String breeId, String dogBreed, String lifeSpan, String breedGroup, String bredFor, String temperament, String imageUrl) {
         this.breeId = breeId;
