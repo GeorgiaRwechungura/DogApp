33f0500988d56da35c30867060091e3659ce466e
Georgia Rwechungura <grwechungura@aimgroup.co.tz>
2020-03-23 16:13:13 +0300

    Detail Fragment with Data Binding

--- app/src/main/java/georgia/com/dogapp/view/DetailFragment.java
+++ app/src/main/java/georgia/com/dogapp/view/DetailFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -15,9 +16,12 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.common.data.DataBufferIterator;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import georgia.com.dogapp.R;
+import georgia.com.dogapp.databinding.FragmentDetailBinding;
 import georgia.com.dogapp.model.DogBreed;
 import georgia.com.dogapp.util.Util;
 import georgia.com.dogapp.viewModel.DetailViewModel;
@@ -25,53 +29,40 @@ import georgia.com.dogapp.viewModel.DetailViewModel;
 public class DetailFragment extends Fragment {
 
     private DetailViewModel viewModel;
-    private  int dogUid;
-    @BindView(R.id.dogImage)
-    ImageView dogImage;
-    @BindView(R.id.dogName)
-    TextView dogName;
-    @BindView(R.id.dogPurpose)
-    TextView dogPurpose;
-    @BindView(R.id.dogTemperament)
-    TextView dogTemperament;
-    @BindView(R.id.dogLifeSpan)
-    TextView dogLifeSpan;
+    private int dogUid;
+    private FragmentDetailBinding binding;
 
     public DetailFragment() {
         // Required empty public constructor
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_detail, container, false);
-        ButterKnife.bind(this, view);
-        return view;
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false);
+        this.binding = binding;
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        if(getArguments()!=null){
-            dogUid=DetailFragmentArgs.fromBundle(getArguments()).getDogUid();
+        if (getArguments() != null) {
+            dogUid = DetailFragmentArgs.fromBundle(getArguments()).getDogUid();
         }
-        viewModel= ViewModelProviders.of(this).get(DetailViewModel.class);
+        viewModel = ViewModelProviders.of(this).get(DetailViewModel.class);
         viewModel.fetch(dogUid);
         observeViewModel();
     }
 
-    private void observeViewModel(){
-   viewModel.dogLiveData.observe(this, dogBreed -> {
-       if(dogBreed!=null && dogBreed instanceof DogBreed && getContext()!=null){
-           dogName.setText(dogBreed.dogBreed);
-           dogLifeSpan.setText(dogBreed.lifeSpan);
-           dogTemperament.setText(dogBreed.temperament);
-           dogPurpose.setText(dogBreed.bredFor);
+    private void observeViewModel() {
+        viewModel.dogLiveData.observe(this, dogBreed -> {
+            if (dogBreed != null && dogBreed instanceof DogBreed && getContext() != null) {
+                binding.setDog(dogBreed);
 
-           if(dogBreed.imageUrl!=null){
-               Util.loadImage(dogImage,dogBreed.imageUrl,new CircularProgressDrawable(getContext()));
-           }
-       }
-   });
+
+            }
+
+        });
     }
 }
