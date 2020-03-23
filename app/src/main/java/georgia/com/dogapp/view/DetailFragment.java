5026e9dd84bba8e3f987c49e416baa44b0110474
Georgia Rwechungura <grwechungura@aimgroup.co.tz>
2020-03-23 16:36:57 +0300

    Detail Fragment with Data Binding

--- app/src/main/java/georgia/com/dogapp/view/DetailFragment.java
+++ app/src/main/java/georgia/com/dogapp/view/DetailFragment.java
@@ -8,18 +8,11 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.google.android.gms.common.data.DataBufferIterator;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import georgia.com.dogapp.R;
 import georgia.com.dogapp.databinding.FragmentDetailBinding;
 import georgia.com.dogapp.model.DogBreed;
