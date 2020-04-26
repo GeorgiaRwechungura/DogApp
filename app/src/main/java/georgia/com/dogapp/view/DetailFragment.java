8f15be2fa58e7db4a317e78173c9e1ad5e6a7490
Georgia Rwechungura <grwechungura@aimgroup.co.tz>
2020-04-26 17:19:18 +0300

    Sending SMS

--- app/src/main/java/georgia/com/dogapp/view/DetailFragment.java
+++ app/src/main/java/georgia/com/dogapp/view/DetailFragment.java
@@ -1,22 +1,28 @@
 package georgia.com.dogapp.view;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.palette.graphics.Palette;
 
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.CustomTarget;
@@ -24,8 +30,10 @@ import com.bumptech.glide.request.transition.Transition;
 
 import georgia.com.dogapp.R;
 import georgia.com.dogapp.databinding.FragmentDetailBinding;
+import georgia.com.dogapp.databinding.SendSmsDialogBinding;
 import georgia.com.dogapp.model.DogBreed;
 import georgia.com.dogapp.model.DogPalette;
+import georgia.com.dogapp.model.SmsInfo;
 import georgia.com.dogapp.util.Util;
 import georgia.com.dogapp.viewModel.DetailViewModel;
 
@@ -34,6 +42,8 @@ public class DetailFragment extends Fragment {
     private DetailViewModel viewModel;
     private int dogUid;
     private FragmentDetailBinding binding;
+    private Boolean sendSmsStarted=false;
+    private DogBreed currentDog;
 
     public DetailFragment() {
         // Required empty public constructor
@@ -64,6 +74,7 @@ public class DetailFragment extends Fragment {
     private void observeViewModel() {
         viewModel.dogLiveData.observe(this, dogBreed -> {
             if (dogBreed != null && dogBreed instanceof DogBreed && getContext() != null) {
+                currentDog=dogBreed;
                 binding.setDog(dogBreed);
                 if(dogBreed.imageUrl!=null){
                     setupBackgroundColor(dogBreed.imageUrl);
@@ -102,6 +113,57 @@ public class DetailFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-       // inflater.inflate(R.menu.);
+        inflater.inflate(R.menu.detail_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_send_sms:{
+                if(!sendSmsStarted){
+                    sendSmsStarted=true;
+                    ( (MainActivity)getActivity()).checkSmsPermission();
+                }
+
+
+            }
+            break;
+            case R.id.action_share:{
+                Toast.makeText(getContext(),"Action Share",Toast.LENGTH_LONG).show();
+
+            }
+            break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onPermissionResult(Boolean permissionGranted) {
+        if(isAdded() && sendSmsStarted && permissionGranted){
+            SmsInfo smsInfo=new SmsInfo("",currentDog.dogBreed + "bred for "+ currentDog.bredFor,currentDog.imageUrl);
+            SendSmsDialogBinding dialogBinding=DataBindingUtil.inflate(
+                    LayoutInflater.from(getContext()),
+                    R.layout.send_sms_dialog,
+                    null,
+                    false
+            );
+            new AlertDialog.Builder(getContext())
+                    .setView(dialogBinding.getRoot())
+                    .setPositiveButton("Send SMS", ((dialog, which) -> {
+                        if(!dialogBinding.smsDestination.getText().toString().isEmpty()) {
+                            smsInfo.to = dialogBinding.smsDestination.getText().toString();
+                            sendSms(smsInfo);
+                        }
+                    }));
+
+
+        }
+
+    }
+    private void sendSms(SmsInfo smsInfo) {
+        Intent intent = new Intent(getContext(), MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(getContext(), 0, intent, 0);
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(smsInfo.to, null, smsInfo.text, pi, null);
     }
 }
--- app/src/main/java/georgia/com/dogapp/view/MainActivity.java
+++ app/src/main/java/georgia/com/dogapp/view/MainActivity.java
@@ -1,17 +1,30 @@
 package georgia.com.dogapp.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import georgia.com.dogapp.R;
 
 public class MainActivity extends AppCompatActivity {
     NavController navController;
+    private static  final int PERMISSION_SEND_SMS=32;
+    private Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +32,73 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         navController= Navigation.findNavController(this,R.id.fragment);
         NavigationUI.setupActionBarWithNavController(this,navController);
+        fragment= getSupportFragmentManager().findFragmentById(R.id.fragment);
     }
 
     @Override
     public boolean onSupportNavigateUp() {
 
         return NavigationUI.navigateUp(navController,(DrawerLayout) null);
+    }
+
+   public  void checkSmsPermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)!= PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.SEND_SMS)){
+                new AlertDialog.Builder(this)
+                        .setTitle("Send Sms Permission")
+                        .setMessage("This app require permission to send sms")
+                        .setPositiveButton("Ask me", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                requestSmsPermission();
+
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                notifyDetailFragment(false);
+                            }
+                        })
+                        .show();
+
+
+            }else{
+                requestSmsPermission();
+            }
+
+        }else{
+            notifyDetailFragment(true);
+        }
+
+    }
+
+    private void requestSmsPermission() {
+        String [] permissions={Manifest.permission.SEND_SMS};
+        ActivityCompat.requestPermissions(this,permissions, PERMISSION_SEND_SMS);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode){
+            case PERMISSION_SEND_SMS:{
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    notifyDetailFragment(true);
+                }else {
+                    notifyDetailFragment(false);
+                }
+
+                break;
+            }
+
+        }
+    }
+
+    private void notifyDetailFragment(Boolean permissionGranted) {
+        Fragment activeFragment=fragment.getChildFragmentManager().getPrimaryNavigationFragment();
+        if(activeFragment instanceof DetailFragment){
+            ( (DetailFragment) activeFragment).onPermissionResult(permissionGranted);
+        }
     }
 }
